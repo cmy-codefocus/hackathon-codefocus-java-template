9cb03aeb39792c26379d181226a2be9bbadc3d34
Carlos Duarte <c.duarte@xgeeks.io>
2021-03-05 09:55:33 +0000

    Add silver arrow endpoint

---
+++ src/main/java/io/cloudmobility/codefocus/silverarrow/SilverArrowController.java
@@ -0,0 +1,25 @@
+package io.cloudmobility.codefocus.silverarrow;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path = "silver-arrow")
+public class SilverArrowController {
+
+    @Value("silverarrow.txt")
+    private ClassPathResource resource2;
+
+    @GetMapping
+    public String getSilverArrow() throws IOException {
+        var content = new String(resource2.getInputStream().readAllBytes());
+
+        return "<pre>" + content + "</pre>";
+    }
+
+}
